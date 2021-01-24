edcf8b7d642eeda20dbb8bd117369e7eb49e66d8
Mathew-B <mathew.bruhn21@ycdsbk12.ca>
2021-01-24 13:34:18 -0500

    commit

--- src/basic/HelloWorld.java
+++ src/basic/HelloWorld.java
@@ -3,9 +3,10 @@ package basic;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
  
 public class HelloWorld extends Application {
@@ -15,20 +16,31 @@ public class HelloWorld extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello World!");
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
+        primaryStage.setTitle("Data Visulization");
+        Button btn1 = new Button();
+        btn1.setText("button 1");
+        btn1.setPrefSize(100, 20);
+        Button btn2 = new Button();
+        btn2.setText("button 2");
+        btn2.setPrefSize(100, 20);
+        btn1.setOnAction(new EventHandler<ActionEvent>() {
  
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                //somecode
             }
         });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
+        btn2.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                //somecode
+            }
+        });
+        VBox Vbox = new VBox();
+        Vbox.setPadding(new Insets(15, 12, 15, 12));
+        Vbox.setSpacing(10);
+        Vbox.getChildren().addAll(btn1,btn2);
+        primaryStage.setScene(new Scene(Vbox, 300, 250));
         primaryStage.show();
     }
 }
