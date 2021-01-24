6718e54acc39ba254247402879a2590bfb6b6075
Mathew-B <mathew.bruhn21@ycdsbk12.ca>
2021-01-24 14:05:36 -0500

    commit

--- src/basic/HelloWorld.java
+++ src/basic/HelloWorld.java
@@ -1,12 +1,22 @@
 package basic;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.PieChart;
+import javafx.scene.chart.XYChart;
 import javafx.stage.Stage;
  
 public class HelloWorld extends Application {
@@ -18,29 +28,158 @@ public class HelloWorld extends Application {
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Data Visulization");
         Button btn1 = new Button();
-        btn1.setText("button 1");
+        btn1.setText("Show Bar Chart");
         btn1.setPrefSize(100, 20);
         Button btn2 = new Button();
-        btn2.setText("button 2");
+        btn2.setText("Show Pie Chart");
         btn2.setPrefSize(100, 20);
         btn1.setOnAction(new EventHandler<ActionEvent>() {
  
             @Override
             public void handle(ActionEvent event) {
-                //somecode
-            }
+                int NonGamenum;  
+                int Indienum;  
+                int Actionnum;  
+                int Adventurenum;  
+                int Casualnum;
+                int Strategynum;
+                int RPGnum;
+                int Simulationnum;
+                int EarlyAccessnum;
+                int FreeToPlaynum;
+                int Sportsnum;
+                int Racingnum;
+                int MassivelyMultiplayernum;   
+               
+                CategoryAxis xaxis= new CategoryAxis();  
+                NumberAxis yaxis = new NumberAxis(0,200,10);  
+                xaxis.setLabel("Genre");  
+                yaxis.setLabel("Total Games Sold");  
+            
+                BarChart<String,Float> bar = new BarChart(xaxis,yaxis);  
+                bar.setTitle("Top Selling Games Of 2020");  
+                  
+                //Configuring Series for XY chart   
+                XYChart.Series<String,Float> series = new XYChart.Series<>();  
+                series.getData().add(new XYChart.Data("NonGame",00));  
+                series.getData().add(new XYChart.Data("Indie",00));  
+                series.getData().add(new XYChart.Data("Action",00));  
+                series.getData().add(new XYChart.Data("Adventure",00));
+                series.getData().add(new XYChart.Data("Casual",00));  
+                series.getData().add(new XYChart.Data("Strategy",00));  
+                series.getData().add(new XYChart.Data("RPG",00));
+                series.getData().add(new XYChart.Data("Simulation",00));  
+                series.getData().add(new XYChart.Data("EarlyAccess",00));  
+                series.getData().add(new XYChart.Data("FreeToPlay",00));  
+                series.getData().add(new XYChart.Data("Sports",00));  
+                series.getData().add(new XYChart.Data("Racing",00));
+                series.getData().add(new XYChart.Data("MassivelyMultiplayer",00));      
+                  
+             
+                bar.getData().add(series); 
+
+                Button btn3 = new Button();
+                btn3.setText("Back");
+                btn3.setPrefSize(100, 20);
+                VBox Vbox = new VBox();
+                Vbox.setPadding(new Insets(15, 12, 15, 12));
+                Vbox.setSpacing(10);
+                Vbox.getChildren().addAll(btn3);
+                btn3.setOnAction(new EventHandler<ActionEvent>() {
+
+                    @Override
+                    public void handle(ActionEvent arg0) {
+                    VBox Vbox = new VBox();
+                    Vbox.setPadding(new Insets(15, 12, 15, 12));
+                    Vbox.setSpacing(10);
+                    Vbox.getChildren().addAll(btn1,btn2);
+                    primaryStage.setScene(new Scene(Vbox, 600, 500));
+                    primaryStage.show();
+                    }
+                 
+                    
+                });
+             
+                Group root = new Group();  
+                root.getChildren().addAll(bar,btn3);  
+                Scene scene = new Scene(root,600,500);  
+                primaryStage.setScene(scene);  
+                primaryStage.setTitle("BarChart");  
+                primaryStage.show();      
+            } 
         });
         btn2.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                //somecode
-            }
+                
+                int NonGamenum;  
+                int Indienum;  
+                int Actionnum;  
+                int Adventurenum;  
+                int Casualnum;
+                int Strategynum;
+                int RPGnum;
+                int Simulationnum;
+                int EarlyAccessnum;
+                int FreeToPlaynum;
+                int Sportsnum;
+                int Racingnum;
+                int MassivelyMultiplayernum;
+
+                    Scene scene = new Scene(new Group());
+					primaryStage.setTitle("Total Games sold 2020");
+                    primaryStage.setWidth(600);
+                    primaryStage.setHeight(500);
+             
+                    ObservableList<PieChart.Data> pieChartData =
+                            FXCollections.observableArrayList(
+                            new PieChart.Data("NonGame", 13),
+                            new PieChart.Data("Indie", 25),
+                            new PieChart.Data("Action", 10),
+                            new PieChart.Data("Adventure", 22),
+                            new PieChart.Data("Casual", 30),
+                            new PieChart.Data("Strategy", 21),
+                            new PieChart.Data("RPG", 23),
+                            new PieChart.Data("Simulation", 11),
+                            new PieChart.Data("EarlyAccess", 9),
+                            new PieChart.Data("FreeToPlay", 10),
+                            new PieChart.Data("Sports", 29),
+                            new PieChart.Data("Racing", 24),
+                            new PieChart.Data("MassivelyMultiplayer", 26));
+                    final PieChart chart = new PieChart(pieChartData);
+                    chart.setTitle("Total Games sold 2020");
+
+                    Button btn3 = new Button();
+                btn3.setText("Back");
+                btn3.setPrefSize(100, 20);
+                VBox Vbox = new VBox();
+                Vbox.setPadding(new Insets(15, 12, 15, 12));
+                Vbox.setSpacing(10);
+                Vbox.getChildren().addAll(btn3);
+                btn3.setOnAction(new EventHandler<ActionEvent>() {
+
+                    @Override
+                    public void handle(ActionEvent arg0) {
+                    VBox Vbox = new VBox();
+                    Vbox.setPadding(new Insets(15, 12, 15, 12));
+                    Vbox.setSpacing(10);
+                    Vbox.getChildren().addAll(btn1,btn2);
+                    primaryStage.setScene(new Scene(Vbox, 600, 500));
+                    primaryStage.show();
+                    }
+                 
+                    
+                });
+            
+                    ((Group) scene.getRoot()).getChildren().addAll(chart,btn3);
+                    primaryStage.setScene(scene);
+                }
         });
         VBox Vbox = new VBox();
         Vbox.setPadding(new Insets(15, 12, 15, 12));
         Vbox.setSpacing(10);
         Vbox.getChildren().addAll(btn1,btn2);
-        primaryStage.setScene(new Scene(Vbox, 300, 250));
+        primaryStage.setScene(new Scene(Vbox, 600, 500));
         primaryStage.show();
     }
 }
