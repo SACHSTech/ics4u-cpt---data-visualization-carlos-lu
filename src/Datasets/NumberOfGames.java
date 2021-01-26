2ebb3bd848a8407faaaa266bf3cbf782bf29a046
Carlos_EOL <74547522+CarlosEOL@users.noreply.github.com>
2021-01-26 13:39:06 -0500

    String array works

--- src/Datasets/NumberOfGames.java
+++ src/Datasets/NumberOfGames.java
@@ -74,7 +74,7 @@ public class NumberOfGames extends Application{
         }
 
         gamesCSV.close(); // Games Object created, CSV file not needed.
-        System.out.println(Games[0].getQueryID());
+        System.out.println(string[0]);
 
         /*
          * Rest of the things are just sorting
